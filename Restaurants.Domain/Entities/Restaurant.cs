bc3c85676df5ca9b8e3fab2707d9d5198bd7e9c1
Kamil Surlas <ks303231@student.polsl.pl>
2024-05-03 15:36:19 +0200

    Dodaj pliki projektów.

---
+++ Restaurants.Domain/Entities/Restaurant.cs
@@ -0,0 +1,16 @@
+﻿namespace Restaurants.Domain.Entities
+{
+    public class Restaurant
+    {
+        public int RestaurantId { get; set; }
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public string Category { get; set; } = default!;
+        public bool HasDelivery { get; set; } = default!;
+        public string ContactEmail { get; set; } = default!;
+        public string ContactNumber { get; set; } = default!;
+        public Address Address { get; set; } = default!;
+        public List<Dish> Dishes { get; set; } = new();
+
+    }
+}
